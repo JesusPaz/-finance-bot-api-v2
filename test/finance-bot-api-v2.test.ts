b904be878b4b4f398acb113eec26bcd4081cb769
Jesus Paz <jeherpabo@gmail.com>
2025-10-25 20:11:03 -0500

    Initial commit

---
+++ test/finance-bot-api-v2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FinanceBotApiV2 from '../lib/finance-bot-api-v2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/finance-bot-api-v2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FinanceBotApiV2.FinanceBotApiV2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
